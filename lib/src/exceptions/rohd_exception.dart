42d1b63accbb1c14b0bf55b4c4db4996c747b630
Yao Jing Quek <yao.jing.quek@intel.com>
2022-12-13 16:48:49 +0800

    Add comment for rohd exception

--- lib/src/exceptions/rohd_exception.dart
+++ lib/src/exceptions/rohd_exception.dart
@@ -1,5 +1,15 @@
+/// Copyright (C) 2022 Intel Corporation
+/// SPDX-License-Identifier: BSD-3-Clause
+///
+/// rohd_exception.dart
+/// An exception class intended to trace the exception is from ROHD.
+///
+/// 2022 November 17
+/// Author: Yao Jing Quek <yao.jing.quek@intel.com>
+///
+
 abstract class RohdException implements Exception {
-  ///
+  /// The custom exception message that intent to notify user.
   late String message;
 
   @override
