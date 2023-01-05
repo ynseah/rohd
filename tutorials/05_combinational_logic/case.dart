e9d7783467b75eebcad3aa6413bd6d2eed1becba
Yao Jing Quek <yao.jing.quek@intel.com>
2023-01-05 09:25:36 +0800

    Added case implementation

--- tutorials/05_combinational_logic/case.dart
+++ tutorials/05_combinational_logic/case.dart
@@ -2,6 +2,8 @@ import 'package:rohd/rohd.dart';
 
 Case truthTableCase(
         Logic a, Logic b, Logic carryIn, Logic sum, Logic carryOut) =>
-    Case(
-      [a, b],
-    );
+    Case([a, b, carryIn].swizzle(), [
+      CaseItem(Const(bin('000')), [
+        carryOut < 0,
+      ]),
+    ]);
--- tutorials/06_sequential_logic/d_ff.dart
+++ tutorials/06_sequential_logic/d_ff.dart
@@ -1,14 +1,15 @@
 import 'package:rohd/rohd.dart';
 
 class DFlipFlop extends Module {
-  DFlipFlop(Logic data, Logic reset) {
+  DFlipFlop(Logic data, Logic reset, Logic clk) {
     // declare input and output
     data = addInput('d', data);
     reset = addInput('reset', reset);
+    clk = addInput('clk', clk);
 
     final q = addOutput('q');
 
-    Sequential(SimpleClockGenerator(10).clk, [
+    Sequential(clk, [
       If(reset, then: [q < 0], orElse: [q < data])
     ]);
   }
@@ -19,9 +20,13 @@ class DFlipFlop extends Module {
 void main() async {
   final data = Logic();
   final reset = Logic();
+  final clk = SimpleClockGenerator(10).clk;
 
-  final dff = DFlipFlop(data, reset);
+  final dff = DFlipFlop(clk, data, reset);
   await dff.build();
 
   print(dff.generateSynth());
+
+  // Add some checks
+  // Start the Simulator and give maximum simulation time
 }
